9fc7818d89a0d09f4e54bffd512445075f06c3d6
GERFSHARP <angiecsb982@gmail.com>
2020-10-15 13:38:46 -0500

    rutas

--- routes/api/teacher_eval/api.php
+++ routes/api/teacher_eval/api.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\TeacherEval\StudentEvaluationController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-<<<<<<< HEAD
 
 Route::apiResource('evaluation_types', EvaluationTypeController::class);
 Route::apiResource('student_evaluations', StudentEvaluationController::class); 
@@ -25,11 +24,3 @@ Route::apiResource('self_evaluations', SelfEvaluationController::class);
 
 
 
-
-
-// Route::get('getTipoEvaluacion','TeacherEval\StudentEvaluationController@getTipoEvaluacion');
-// Route::get('index','TeacherEval\StudentEvaluationController@index');
-=======
-Route::apiResource('teacher_eval', 'App\Http\Controllers\TeacherEval\SelfAppraisalController');
-Route::apiResource('teacher_eval_type', 'App\Http\Controllers\TeacherEval\EvaluationTypeController');
->>>>>>> e6a024fa36c780b4b4b9f3451952b4f75f743fe9
