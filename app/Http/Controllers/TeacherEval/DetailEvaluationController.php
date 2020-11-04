d534fe16d21799b37cd66ebb2743610db4ac2cdf
jeffymono <jeffymona123@gmail.com>
2020-11-03 19:58:37 -0500

    Merge branch 'master' of https://github.com/AngieJoseth/backend-evaluacion-docente into master

--- app/Http/Controllers/TeacherEval/DetailEvaluationController.php
+++ app/Http/Controllers/TeacherEval/DetailEvaluationController.php
@@ -7,46 +7,110 @@ use Illuminate\Http\Request;
 use App\Models\Ignug\State;
 use App\Models\Ignug\Teacher;
 use App\Models\TeacherEval\DetailEvaluation;
+use App\Models\TeacherEval\EvaluationType;
+use App\Models\TeacherEval\Evaluation;
 
 class DetailEvaluationController extends Controller
 {
     public function index()
     {
-         return DetailEvaluation::all();
+        return DetailEvaluation::all();
     }
 
     public function show($id)
     {
-        $detailEvaluation =  DetailEvaluation::findOrFail($id);
-//        $catalogue =  Catalogue::where('id',$id)->get();
+       $detailEvaluation =  DetailEvaluation::findOrFail($id);
         return response()->json([
             'data' => [
                 'detailEvaluation' => $detailEvaluation
-            ]]);
+        ]]);
     }
 
     public function store(Request $request)
     {
         $data = $request->json()->all();
         
-        $dataDetailEvaluation = $data['detailEvaluation'];
-        $dataState = $data['state'];
+        $dataEvaluationType = $data['evaluation_type'];
+        $dataTeacher = $data['teacher'];
+        $dataEvaluators = $data['evaluators'];
 
-        $detailEvaluation = new DetailEvaluation();
-        $detailEvaluation->detail_evaluationable_id= $dataDetailEvaluation['detail_evaluationable_id'];
-        $detailEvaluation->detail_evaluationable_type = $dataDetailEvaluation['detail_evaluationable_type'];
+        $evaluation = new Evaluation();
+ 
+        $state = State::where('code', '1')->first();
+        $teacher = Teacher::findOrFail($dataTeacher['id']);
+        $evaluationType = EvaluationType::findOrFail($dataEvaluationType['id']);
 
-        $state = State::findOrFail($dataState['id']);
-        $teacher = Teacher::findOrFail($dataDetailEvaluation['detail_evaluationable_id']);
+        $evaluation->state()->associate($state);
+        $evaluation->teacher()->associate($teacher);
+        $evaluation->evaluationType()->associate($evaluationType);
+        $evaluation->save();
 
-        $detailEvaluation->state()->associate($state);
-        $teacher->detailEvaluations()->save($detailEvaluation);
-
+        foreach($dataEvaluators as $evaluator)
+        {
+            $detailEvaluation = new DetailEvaluation;
+            $detailEvaluation->state()->associate($state);
+            $detailEvaluation->detailEvaluationable()->associate(Teacher::findOrFail($evaluator['id']));
+            $detailEvaluation->evaluation()->associate($evaluation); 
+            $detailEvaluation->save();   
+        }
+       
         return response()->json([
         'data' => [
-            'detailEvaluation' => $teacher
-        ]
-    ], 201);
+            'detailEvaluation' => $evaluation
+            ]
+        ], 201);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $data = $request->json()->all();
+        
+        $dataEvaluationType = $data['evaluation_type'];
+        $dataTeacher = $data['teacher'];
+        $dataEvaluators = $data['evaluators'];
+        $dataState = $data['state'];
+        
+        $evaluation = Evaluation::findOrFail($id);
+        $state = State::findOrFail($dataState['id']);
+        $teacher = Teacher::findOrFail($dataTeacher['id']);
+        $evaluationType = EvaluationType::findOrFail($dataEvaluationType['id']);
+
+        $evaluation->state()->associate($state);
+        $evaluation->teacher()->associate($teacher);
+        $evaluation->evaluationType()->associate($evaluationType);
+        $evaluation->save();
+
+        foreach($dataEvaluators as $evaluator)
+        {
+            $detailEvaluation = DetailEvaluation::where('evaluation_id', $id)->first();
+            $detailEvaluation->detailEvaluationable()->associate(Teacher::findOrFail($evaluator['id']));
+            $detailEvaluation->save();   
+        }
+
+        return response()->json([
+            'data' => [
+                'evaluation' => $detailEvaluation
+            ]
+        ], 201);
+    }
+    public function destroy($id)
+    {
+        $evaluation = Evaluation::findOrFail($id);
+
+        $evaluation->state_id = '3';
+        $evaluation->save();
+
+        $detailEvaluations = DetailEvaluation::where('evaluation_id',$id)->get();
+        foreach($detailEvaluations as $detailEvaluation){
+            $detailEvaluation->state_id = '3';
+            $detailEvaluation->save();
+        }
+
+        return response()->json([
+            'data' => [
+                'detail_evaluation' => $detailEvaluation
+            ]
+        ], 201);
     }
     
 }
